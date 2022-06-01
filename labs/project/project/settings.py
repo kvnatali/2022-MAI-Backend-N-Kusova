0a4c778ffc4bce26e07e0e6f72e81796c027534a
kvnatali <hedgehoginafog@gmail.com>
2022-06-01 22:31:59 +0300

    laba 4

--- labs/project/project/settings.py
+++ labs/project/project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# Для лабы 4:
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'study',
+        'USER': 'user1',
+        'PASSWORD': '1qaz@WSX',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
